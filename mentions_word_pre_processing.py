85173d1b79d917ae8e70155f2824e26ef8df96d3
Tomas Ye <t.ye@sap.com>
2018-12-11 09:19:56 +0100

    sdgsdgds

--- mentions_word_pre_processing.py
+++ mentions_word_pre_processing.py
@@ -39,19 +39,30 @@ stop_words_cz += [
     'tech'
 ]
 
+
 def create_feature_matrix(matrix, tokens) -> pd.DataFrame:
-        doc_names = ['Doc {:d}'.format(i) for i, value in enumerate(matrix)]
-        return pd.DataFrame(data=matrix, index=doc_names, columns=tokens).transpose()
+    doc_names = ['mention_{:d}'.format(i) for i, value in enumerate(matrix)]
+    return pd.DataFrame(data=matrix, index=doc_names, columns=tokens).transpose()
 
 
-dataset = pd.read_csv('cleaner_data.csv')
-mentions = dataset['Obsah zmínek'].dropna()
-vectorizer = CountVectorizer(stop_words=stop_words_cz)
-matrix_raw = vectorizer.fit_transform(mentions).todense()
-feature_names = vectorizer.get_feature_names()
-feature_matrix = create_feature_matrix(matrix_raw, feature_names)
-num_of_columns = len(list(feature_matrix.columns.values))
-ones = pd.DataFrame(data=[1 for i in range(0, num_of_columns)], index=feature_matrix.columns, columns=['Frequency'])
-counts_in_mentions = feature_matrix.dot(ones)
-counts_in_mentions = counts_in_mentions.sort_values(by=['Frequency'], ascending=False)
-counts_in_mentions.to_csv('counts_in_mentions.csv')
+# TODO implement these methods so that the feature matrix can be created more easily
+def get_indicator_matrix(feature_matrix):
+    return feature_matrix.apply(lambda x: x > 0).astype(dtype=int)
+
+
+def get_relevance_indicator_matrix(indicator_matrix):
+    return indicator_matrix.loc[:, indicator_matrix.apply(func=lambda x: x[RELEVANT] == 1, axis=0)]
+
+
+def get_irrelevance_indicator_matrix(indicator_matrix):
+    return indicator_matrix.loc[:, indicator_matrix.apply(func=lambda x: x[RELEVANT] == 0, axis=0)]
+
+
+def run_vectorizer_without_preprocessing():
+    dataset = pd.read_csv('cleaner_data.csv')
+    mentions = dataset['Obsah zmínek'].dropna()
+    vectorizer = CountVectorizer(stop_words=stop_words_cz)
+    matrix_raw = vectorizer.fit_transform(mentions).todense()
+    feature_names = vectorizer.get_feature_names()
+    feature_matrix = create_feature_matrix(matrix_raw, feature_names)
+
